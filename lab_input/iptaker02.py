a8ba402ab7fff51d49da06bfd1b70cf310e1573b
sfeeser <sfeeser@gmail.com>
2022-08-22 20:56:44 +0000

    push

---
+++ lab_input/iptaker02.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def main():
+    user_input = input("Please enter an IPv4 IP address:")
+    vendor = input("Please enter the vendor name:")
+    print("You chose a device from:", vendor, "that will become", user_input)
+
+
+
+main()
+
